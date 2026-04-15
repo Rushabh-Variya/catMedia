112887d382985c0e9fc963b4c43152e85924c53b
Rushabh Variya <rkv20052712@gmail.com>
2026-04-15 11:53:56 +0530

    Initial Commit

---
+++ catMedia/catMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  catMediaApp.swift
+//  catMedia
+//
+//  Created by Rushabh Variya on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct catMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
